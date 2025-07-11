40e9ecfe89878a3f7b01dfa5af4a11a275651b94
mariaelisaaraya <maria.elisa.araya.work@gmail.com>
2025-07-10 23:54:28 -0300

    Guardando cambios antes de rebase

--- contracts/calculadora/src/lib.rs
+++ contracts/calculadora/src/lib.rs
@@ -1,23 +1,23 @@
 #![no_std]
 
+use soroban_sdk::{contract, contractimpl, Env, symbol_short, Symbol};
 
-use soroban_sdk::{contract, contractimpl,  Env, symbol_short, Symbol};
 const RESULTADO: Symbol = symbol_short!("RESULTADO");
+
 #[contract]
 pub struct Contract;
-
 
 #[contractimpl]
 impl Contract {
     
     pub fn sumar(env: Env, a:i128, b:i128) -> i128 {
       //Implementar función que sume dos números
-      return 20;
+      return 30;
     }
 
     pub fn resultado_anterior(env: Env) -> i128 {
            //Implementar función que retorne el valor anterior
-            return 20;
+            return 30;
     }
 }
 
--- contracts/calculadora/src/test.rs
+++ contracts/calculadora/src/test.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{vec, Env, String};
+use soroban_sdk::Env;
 
 #[test]
 fn test() {
